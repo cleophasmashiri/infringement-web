ff1e918e84902f746ee2b52bc1c65e069f7c5cae
Cleophas Mashiri <cleophas.mashiri@standardbank.co.za>
2020-08-30 15:45:54 +0200

    Updated

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -26,7 +26,20 @@ export class RegisterComponent implements AfterViewInit, OnInit {
   errorUserExists = false;
   success = false;
 
-  registerForm?: FormGroup;
+  registerForm = this.fb.group({
+    login: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(50),
+        Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
+      ],
+    ],
+    email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+  });
 
   constructor(
     private languageService: JhiLanguageService,
